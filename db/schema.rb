6637b560dc8efc86f1b92025cb4512dca0236140
AfteRRaIN-Sys <phet610@hotmail.com>
2021-10-09 19:46:17 +0700

    added Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_070956) do
+ActiveRecord::Schema.define(version: 2021_10_09_104734) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "msg"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_10_07_070956) do
     t.integer "age"
   end
 
+  add_foreign_key "posts", "users"
 end
